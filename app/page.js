f1ab69e7c75f7230dd5a9d79f00640de9ab19725
Viraj <virajmohit7@gmail.com>
2023-06-17 19:45:55 -0700

    textarea edited

--- app/page.js
+++ app/page.js
@@ -10,10 +10,10 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className='flex flex-row h-screen w-full space-x-28 mx-8 '>
-        <div className='basis-1/2 border border-black w-full h-5/6 p-5 bg-white text-black z-50 rounded-xl'>
+        <div className='basis-1/2 border border-black w-full max-h-full h-full p-5 bg-white text-black z-50 rounded-xl'>
           <h4>Plots</h4>  
         </div> 
-        <div className='basis-1/2 border border-black w-full h-5/6 p-5 bg-white text-black z-50 rounded-xl'>
+        <div className='basis-1/2 border border-black w-full max-h-full h-full p-5 bg-white text-black z-50 rounded-xl'>
           <UserPrompt></UserPrompt>
         </div>       
       </div>
--- components/userPrompt.js
+++ components/userPrompt.js
@@ -5,9 +5,9 @@ import TextareaAutosize from 'react-textarea-autosize';
 
 const UserPrompt = () => {
     return (
-        <div className='w-full h-fit'>
-            <h4>Username:</h4>
-            <TextareaAutosize className='w-full resize-none mt-0 p-2 rounded-xl' minRows='1' placeholder='Enter Prompt'/>
+        <div className='flex flex-col w-full max-h-full overflow-auto'>
+            <h4 className='bold'>Username:</h4>
+            <TextareaAutosize className='w-full resize-none mt-0 p-2 rounded-xl ' minRows='1' placeholder='Enter Prompt'/>
         </div>
     );
 };
